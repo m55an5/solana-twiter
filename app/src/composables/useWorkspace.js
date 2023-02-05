7fcbaf23618c15a0dda01e0c4b51b0f420fdbfa8
manjot <mssan5@student.monash.edu>
2023-02-05 18:32:26 +1100

    changed to devnet, idl dynamic scirpt, and deployment changes

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,8 +2,9 @@ import { computed } from 'vue'
 import { useAnchorWallet } from 'solana-wallets-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { AnchorProvider, Program } from '@project-serum/anchor'
-import idl from '../../../target/idl/solana_twiter.json'
+import idl from '@/idl/solana_twiter.json'
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 const preflightCommitment = 'processed'
 const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
@@ -14,7 +15,7 @@ export const useWorkspace = () => workspace
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
     // as a fallback commitment level when it is not directly provided on the transaction
-    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const connection = new Connection(clusterUrl, commitment)
     const provider = computed(() => new AnchorProvider(connection, wallet.value, 
                                         { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
@@ -26,3 +27,13 @@ export const initWorkspace = () => {
         program,
     }
 }
+
+// 6ar8YVWcscGV93PfEdkT2R67Np7TAxwtAX7mD6wtQJeK --devnet
+
+
+// anchor.toml
+// [workspace]
+// types = "app/src/idl/"
+
+// it won’t copy the program ID inside the IDL which we need for our workspace
+//  to be check later
--- programs/solana-twiter/src/lib.rs
+++ programs/solana-twiter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6ar8YVWcscGV93PfEdkT2R67Np7TAxwtAX7mD6wtQJeK");
 
 #[program]
 pub mod solana_twiter {
